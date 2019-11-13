79a74550516c0ebe5702ef71871695890f4b4078
aymen0406 <aymenboussaid@gmail.com>
2019-11-13 22:58:22 +0100

    allaaaa commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AnnoncesComponent } from './annonces/annonces.component';
+import { AlaaComponent } from './alaa/alaa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnnoncesComponent
+    AnnoncesComponent,
+    AlaaComponent
   ],
   imports: [
     BrowserModule
